02c135d2e470b97a607686fbec7d7958ee7a9e0c
Aliakbar025 <aliakber.huseynov555@gmail.com>
2024-04-23 19:24:06 +0300

    Sunday

---
+++ Program.cs
@@ -0,0 +1,89 @@
+﻿//1tsk.
+//int[] array = { 1, 2, 3, 4 };
+//for (int i = 0; i < array.Length; i++)
+//{
+//    Console.WriteLine(array[i] );
+//};
+//2tsk.
+//int[] array = { 1, 2, 3, 4, 5 };
+//int sum = 0;
+//for ( int i = 0; i < array.Length; i++)
+//{
+//    sum += array[i];
+//};
+//Console.WriteLine(sum);
+
+//3tsk.
+//int[] arr = { 1, 2, 3, 4, 5 };
+//for (int i = arr.Length - 1; i <= 5; i--)
+//{
+//Console.WriteLine(arr[i]);    
+//}
+
+//4Task.
+
+//int[] arr = { 1, 3, 5, 7, 9 };
+//int max = 0;
+//for (int i = 0; i < arr.Length; i++)
+//{
+//    if (max < arr[i])
+//    {
+//        max = arr[i];
+
+//    }
+
+//}
+//Console.WriteLine(max);
+
+//5tsk.
+
+//int[] arr = {1, 2, 3, 4, 5};
+//int count  = 0; 
+//for (int i = 0; i < arr.Length; i++)
+//{
+//    count++;
+//}
+//Console.WriteLine(count);
+//7tsk.
+//int[] arr = { 0, 0, 5, 0, 3, 6, 0, 8, 0 };
+//int count = 0;
+
+//for (int i = 0; i < arr.Length; i++)
+//{
+//    if (arr[i] == 0)
+//    {
+//        count++;
+//    }
+//}  
+//Console.Write(count);
+//8tsk.
+
+//int[] arr = { -1, 2, -3, -9 };
+//int count = 0;
+//for (int i = 0; i < arr.Length; i++)
+//{
+//    if (arr[i] < 0)
+//        count++;
+//}
+//Console.WriteLine(count);
+
+//9tsk.
+
+//int[] arr = { 1, 2, 4, -8, 0, 9 };
+//int sum = 0;
+//for (int i = 0; i < arr.Length; i++)
+//{
+//    if (arr[i] > 0)
+//        sum += arr[i];
+//}
+//Console.WriteLine(sum);
+
+//10tsk.
+//int[] arr = { 8, 9, 0, 7, 6, -8 };
+//for (int i = 0; i < arr.Length; i++)
+//{
+//    arr[i] = arr[i] * arr[i];
+
+//    Console.Write(arr[i] + " ");
+
+//}
